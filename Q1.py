14fc7db616984fd34ae368f1a825c948291dbac3
Ashwin_21 <59742930+ashwin8121@users.noreply.github.com>
2023-06-28 00:10:34 +0530

    Update Q1.py

--- Q1.py
+++ Q1.py
@@ -5,7 +5,7 @@ for _ in range(N):
    inp = int(input()) # get elements of the array or the list
    lst.append(inp)
 
-maxElement = -1
+maxElement = -9999
 for element in lst[-1::-1]:
     if element > maxElement:
         print(element, end=" ")
